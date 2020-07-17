afeec3258cbd05995867762af3aec3158bb2e6da
ggomjae <ggomjae@naver.com>
2020-07-17 21:24:42 +0900

    2020 07 17 반복문

--- hello.go
+++ hello.go
@@ -4,4 +4,23 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello")
+	var a int = 1
+	b := "oh"
+	fmt.Println(a, b)
+
+	sum := 0
+	for index := 0; index <= 10; index++ {
+		sum += index
+	}
+
+	fmt.Println(sum)
+
+	i := 0
+	var secondSum int = 0
+	for i <= 10 {
+		secondSum += i
+		i++
+	}
+
+	fmt.Println(secondSum)
 }
